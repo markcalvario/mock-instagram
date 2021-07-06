eea90a7cef10e6c688d1652c53fe28f38725ef7d
Mark Anthony Calvario <mac2492@fb.com>
2021-07-06 16:19:02 -0700

    [Instagram][Feature 4.1] Take a Photo 	- add a segue from the instagram logo (compose icon) to another view controller where you can select photo to post 	- user can take a photo if they have a camera 	- if user has no camera access, user can select from camera roll 	- user can enter caption in the text field 	- user can hit the 'cancel' button to go back to the home/feed view controller

---
+++ Instagram/View Controllers/PhotoMapViewController.h
@@ -0,0 +1,16 @@
+//
+//  PhotoMapViewController.h
+//  Instagram
+//
+//  Created by mac2492 on 7/6/21.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PhotoMapViewController : UIViewController <UIImagePickerControllerDelegate, UINavigationControllerDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
